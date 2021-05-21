4cbbe0705b13951fba8d0f69abdaa0da26df0a8b
Torwent <Torwent@users.noreply.github.com>
2021-05-21 19:21:41 +0200

    Update premiere.cmd

--- premiere.cmd
+++ premiere.cmd
@@ -1,22 +1,33 @@
 @echo off
 set /p dirName="Enter Name: "
+
+:: set the full working directory for the video project with the following structure:
+:: yy_mm_dd_project
 set fullDirectory=%date:~8,2%_%date:~3,2%_%date:~0,2%_%dirName%
 
-REM for /f "eol=; tokens=*" %%fullDirectory= in ('powershell Get-Clipboard') do set CLIPBOARD_TEXT=%%fullDirectory
-
 mkdir %fullDirectory%
+
+:: create subfolders
 mkdir %fullDirectory%\resources
 mkdir %fullDirectory%\docs
 mkdir %fullDirectory%\out
 
-for %%f in (template.prproj) do (
-    if "%%~xf"==".prproj" set file="%%f"
+:: look for the template.prproj file in the current working directory and make
+:: a copy of it into the root of the newly created video project directory.
+if exist template.prproj (
+    copy template.prproj %fullDirectory%\%dirName%.prproj
 )
 
-copy %file% %fullDirectory%\%fullDirectory%.prproj
+:: sets up a counter variable
+set /a counter = 0
 
+:: enabledelayedexpansion to update counter variable each loop
+setlocal ENABLEDELAYEDEXPANSION
+
+:: copy each mp4 file into the resources directory in the project folder
 for %%f in (*.mp4) do (
-    if "%%~xf"==".mp4" set file="%%f"
+    move %%f %fullDirectory%\resources\%dirName%!counter!.mp4
+    set /a counter += 1
 )
 
-move %file% %fullDirectory%\resources\video.mp4
+endlocal
